15f0ae2e0ba43c0415a1291ca860a824d9ac396d
Alaamansourn <mansouralaa887@gmail.com>
2021-06-16 19:47:19 +0300

    commit

---
+++ Spring/DateDisplay/src/main/java/com/codingdojo/DateDisplay/DateDisplayApplication.java
@@ -0,0 +1,74 @@
+package com.codingdojo.DateDisplay;
+import java.util.*;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class DateDisplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateDisplayApplication.class, args);
+	}
+	
+
+@RequestMapping("/")
+
+public String show() {
+	
+	
+	
+	
+	
+	return "index.jsp";
+}
+
+
+@RequestMapping("/date")
+public String date(Model model){
+	
+	
+
+	
+	
+	LocalDateTime dateTime =  LocalDateTime.now();
+	DateTimeFormatter local =  DateTimeFormatter.ofPattern("EEEE, ' the' dd 'of' MMMM , yyyy");
+	String mm = dateTime.format(local);
+	
+	 model.addAttribute("date",mm );
+	 
+	return "date.jsp";
+	
+	
+	
+	
+}
+
+@RequestMapping("/time")
+public String time(Model model){
+	
+	
+
+	
+	
+	LocalDateTime dateTime =  LocalDateTime.now();
+	DateTimeFormatter local =  DateTimeFormatter.ofPattern("HH:mm a");
+	String mm = dateTime.format(local);
+	
+	 model.addAttribute("date",mm );
+	 
+	return "date.jsp";
+	
+	
+	
+	
+}
+
+
+}
---
+++ Spring/DateDisplay/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,11 @@
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<c:out value="${2+8}"/>
+
+<html>
+
+
+<a href=date>Date Template</a>
+<a href=time>Time Template</a>
+
+
+</html>
