e296935e56006193ef4a59639099e85afedb57d9
Alaamansourn <mansouralaa887@gmail.com>
2021-06-21 23:31:46 +0300

    commit

---
+++ Spring/mvc1/controllers/BooksApi.java
@@ -0,0 +1,71 @@
+package com.codingdojo.mvc1.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codingdojo.mvc1.models.Book;
+import com.codingdojo.mvc1.services.BookService;
+
+@RestController
+public class BooksApi {
+	
+	
+	
+	 private final BookService bookService;
+	    public BooksApi(BookService bookService){
+	        this.bookService = bookService;
+	    }
+	    
+	    
+	    
+	    @RequestMapping("/api/books")
+	    public List<Book> index() {
+	        return bookService.allBooks();
+	    }
+	    
+	    
+	    
+	    @RequestMapping(value="/api/books/create", method=RequestMethod.POST)
+	    public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") int numOfPages) {
+	    	System.out.println(title);
+	    	System.out.println(title);
+	        Book book = new Book();
+	        book.setLanguage(lang);
+	        book.setTitle(title);
+	        book.setDescription(desc);
+	        
+	        book.setNumberOfPages(numOfPages);
+	        
+	        return bookService.createBook(book);
+	        
+	        
+	       
+	    }
+	    
+	    @RequestMapping("/api/books/{id}")
+	    public Book show(@PathVariable("id") Long id) {
+	        Book book = bookService.findBook(id);
+	        return book;
+	    }
+	    
+	    @RequestMapping("/api/update-books/{id}")
+	    public Book update(@PathVariable("id") Long id,@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") int numOfPages) {
+	    	System.out.println(id);
+	    	
+    	Book book=bookService.findBook(id);
+    	
+	    	book.setTitle(title);
+	    	book.setDescription(desc);
+	    	book.setNumberOfPages(numOfPages);
+	    	book.setLanguage(lang);
+	
+    	return bookService.updateBook(book);
+	 
+	    }
+
+}
---
+++ Spring/mvc1/services/BookService.java
@@ -0,0 +1,56 @@
+package com.codingdojo.mvc1.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.codingdojo.mvc1.models.Book;
+import com.codingdojo.mvc1.repositories.BookRepository;
+
+@Service
+public class BookService {
+	
+	
+	
+	
+	 private final BookRepository bookRepository;
+	    
+	    public BookService(BookRepository bookRepository) {
+	        this.bookRepository = bookRepository;
+	    }
+	    // returns all the books
+	    public List<Book> allBooks() {
+	        return bookRepository.findAll();
+	    }
+	    // creates a book
+	    public Book createBook(Book b) {
+	        return bookRepository.save(b);
+	    }
+	    // retrieves a book
+	    public Book findBook(Long id) {
+	        Optional<Book> optionalBook = bookRepository.findById(id);
+	        if(optionalBook.isPresent()) {
+	            return optionalBook.get();
+	        } else {
+	            return null;
+	        }
+	    }
+	    
+	    
+	    //update a book 
+	    
+	    public Book updateBook(Book b ) {
+	    	return bookRepository.save(b);
+	    	
+	    	
+	    	
+	    	
+	    	
+	    	
+	    	
+	    	
+	    	
+	    }
+
+}
